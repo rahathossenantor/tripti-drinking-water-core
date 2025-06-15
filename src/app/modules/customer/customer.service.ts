6a300589ad387445fd2017b726e2f170f56e7a5b
Md Rahat Hossen Antor <md.rahathossenantor@gmail.com>
2025-06-15 19:20:18 +0600

    Update customer service created

--- src/app/modules/customer/customer.service.ts
+++ src/app/modules/customer/customer.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import mongoose from "mongoose";
 import { Order } from "../order/order.model";
 import { Customer } from "./customer.model";
@@ -18,6 +19,19 @@ const getAllCustomersFromDB = async () => {
     return dbRes;
 };
 
+// update a customer
+const updateCustomerIntoDB = async (id: string, payload: Partial<TCustomer>) => {
+    const dbRes = await Customer.findByIdAndUpdate(
+        id,
+        { $set: payload },
+        { new: true, runValidators: true }
+    );
+    if (!dbRes) {
+        throw new Error("Failed to update customer!");
+    }
+    return dbRes;
+};
+
 // delete a customer
 const deleteCustomerFromDB = async (id: string) => {
     const orders = await Order.find({ customer: id });
@@ -25,7 +39,15 @@ const deleteCustomerFromDB = async (id: string) => {
     const dueOrders = orders.filter((order) => order.paymentStatus === "Due");
     if (dueOrders.length) {
         throw new Error("Cannot delete customer with due orders!");
-    };
+    }
+
+    if (!orders.length) {
+        const deletedCustomer = await Customer.findByIdAndDelete(id);
+        if (!deletedCustomer) {
+            throw new Error("Failed to delete customer!");
+        }
+        return deletedCustomer;
+    }
 
     const session = await mongoose.startSession();
     session.startTransaction();
@@ -34,12 +56,12 @@ const deleteCustomerFromDB = async (id: string) => {
         const deletedOrders = await Order.deleteMany({ customer: id }, { session });
         if (!deletedOrders.deletedCount) {
             throw new Error("Failed to delete orders for this customer!");
-        };
+        }
 
         const deletedCustomer = await Customer.findByIdAndDelete(id, { session });
         if (!deletedCustomer) {
             throw new Error("Failed to delete customer!");
-        };
+        }
 
         await session.commitTransaction();
         await session.endSession();
@@ -48,11 +70,12 @@ const deleteCustomerFromDB = async (id: string) => {
         await session.abortTransaction();
         await session.endSession();
         throw new Error("Failed to delete customer!" + err.message);
-    };
+    }
 };
 
 export const customerServices = {
     createCustomerIntoDB,
     getAllCustomersFromDB,
+    updateCustomerIntoDB,
     deleteCustomerFromDB,
 };
